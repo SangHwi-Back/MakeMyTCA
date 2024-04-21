0bc4b737af1676c8a1269d3be8df809288d183c4
SangHwi-Back <sanghwiback@gmail.com>
2024-04-21 16:21:08 +0900

    initial commit

---
+++ MakeMyTCA/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MakeMyTCA
+//
+//  Created by 백상휘 on 4/21/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
